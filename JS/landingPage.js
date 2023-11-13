b2e7a2684c09bae70f5b101a080609787cf9d0e4
Octenx <jota.idk@icloud.com>
2023-11-13 15:17:58 -0300

    feat: adding JS and some images

---
+++ JS/landingPage.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+    $('#telefone').mask('(00) 00000-0000');
+    
+    $('form').validate({
+        rules: {
+            nome: {
+                required: true
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            telefone: {
+                required: false
+            },
+            mensagem: {
+                required: true
+            }
+        },
+        messages: {
+            nome: 'Por favor, insira o seu nome.',
+            email: 'Insira um E-Mail válido.',
+            mensagem: 'Por favor, insira a sua mensagem.'
+        },
+        submitHandler: function(form) {
+            console.log(form);
+        },
+        invalidHandler: function(evento, validador) {
+            let camposIncorretos = validador.numberOfInvalids();
+            if (camposIncorretos) {
+                alert(`Existem ${camposIncorretos} campos incorretos`);
+            }
+        }
+    });
+});
